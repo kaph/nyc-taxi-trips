3751622c5e8bb36ef8fbda1ac4650ba969637cff
Gibbon <gibbon@furg.br>
2020-09-21 08:45:10 -0300

    upload files to s3

---
+++ import_to_s3_nyc-taxi-trips.cmd
@@ -0,0 +1,4 @@
+aws s3 --kaph_dev cp taxi_small_2009.json s3://kaph-raw/nyc-taxi-trips/
+aws s3 --kaph_dev cp taxi_small_2010.json s3://kaph-raw/nyc-taxi-trips/
+aws s3 --kaph_dev cp taxi_small_2011.json s3://kaph-raw/nyc-taxi-trips/
+aws s3 --kaph_dev cp taxi_small_2012.json s3://kaph-raw/nyc-taxi-trips/
